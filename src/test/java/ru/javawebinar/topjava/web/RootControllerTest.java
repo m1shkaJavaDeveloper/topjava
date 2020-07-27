a440e64942e7036f92bbea126021b24bb5f1ba82
Михаил <m1shka1998@mail.ru>
2020-07-27 17:55:49 +0300

    7_16_soapui_utf8_converter

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -29,4 +29,35 @@ class RootControllerTest extends AbstractControllerTest {
                         }
                 ));
     }
+
+    @Test
+    void testMeal() throws Exception{
+    perform(get("/meals"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("meals"))
+            .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
