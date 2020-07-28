fb44cb8db6b55de3b3e439f85898788844df2a97
Михаил <m1shka1998@mail.ru>
2020-07-28 18:11:45 +0300

    HW07

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,7 +1,55 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 
-@Controller
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = MealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealRestController extends AbstractMealController {
+
+    static final String REST_URL = "/rest/meals";
+
+    @Override
+    @GetMapping("/{id}")
+    public Meal get(@PathVariable int id){
+        return super.get(id);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id){
+        super.delete(id);
+    }
+
+    @GetMapping
+    public List<MealTo> getAll(){
+        return super.getAll();
+    }
+
+    @Override
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Meal meal, @PathVariable int id) {
+        super.update(meal, id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Meal create(@RequestBody Meal meal) {
+        return super.create(meal);
+    }
+
+    @GetMapping("/by")
+    public List<MealTo> getBetween(@DateTimeFormat LocalDate startDate, @DateTimeFormat LocalTime startTime,
+                                   @DateTimeFormat LocalDate endDate, @DateTimeFormat LocalTime endTime){
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
 }
